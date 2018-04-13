22ea741e149f2811ea6188dab1dc2bdcda7d637a
Johanna Latt <j.latt@sap.com>
2018-04-13 15:42:28 -0400

    implemented own process to simulate Kinect and moved that code out of mirror

---
+++ index.py
@@ -0,0 +1,45 @@
+import _thread
+import threading
+import messaging as Messaging
+from messaging import MSG_TO_SERVER_KEYS
+import time
+
+
+Messaging.init()
+_thread.start_new_thread(Messaging.start_sending, ())
+
+
+# Simulate tracking
+def simulate_tracking():
+    while True:
+        for tracking_data_item in sample_tracking_data:
+            while stop_simulating.is_set():
+                pass
+            Messaging.send(MSG_TO_SERVER_KEYS.TRACKING_DATA.name, tracking_data_item)
+            time.sleep(.2)
+
+
+# Sample data
+stop_simulating = threading.Event()
+stop_simulating.set()
+
+sample_tracking_data = open('./data/sample-tracking-data.txt').read().splitlines()
+
+simulation_thread = threading.Thread(target=simulate_tracking)
+simulation_thread.start()
+
+while(True):
+    input_key = input("Press q to quit, t to start the tracking simulation and p to pause the simulation...\n").strip()
+    if input_key == "q":
+        stop_simulating.set()
+        Messaging.send(MSG_TO_SERVER_KEYS.TRACKING_LOST.name, '')
+        break
+    elif input_key == "p":
+        stop_simulating.set()
+        Messaging.send(MSG_TO_SERVER_KEYS.TRACKING_LOST.name, '')
+    elif input_key == "t":
+        if stop_simulating is not None:
+            Messaging.send(MSG_TO_SERVER_KEYS.TRACKING_STARTED.name, '')
+            stop_simulating.clear()
+    else:
+        continue
---
+++ messaging.py
@@ -0,0 +1,52 @@
+import pika
+from enum import Enum
+import configparser
+import queue
+
+
+class MSG_TO_SERVER_KEYS(Enum):
+    TRACKING_STARTED = 1
+    TRACKING_DATA = 2
+    TRACKING_LOST = 3
+
+
+def init():
+    # Save the queue
+    global queue
+    queue = queue.Queue()
+
+    # Create a local messaging connection
+    Config = configparser.ConfigParser()
+    Config.read('./config/kinect_config.ini')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=Config.get('General', 'messaging_ip')))
+
+    global __channel
+    __channel = connection.channel()
+
+    # Create an exchange for the mirror-messages - type is direct so we can distinguish the different messages
+    __channel.exchange_declare(exchange='from-kinect', exchange_type='direct')
+
+
+def start_sending():
+    while True:
+        item = queue.get()
+        if item is None:
+            continue
+        try:
+            __channel.basic_publish(exchange='from-kinect',
+                              routing_key=item['key'],
+                              body=item['body'])
+            print("[info] Sent {}: {}".format(item['key'], item['body'][0:50]))
+        except pika.exceptions.ConnectionClosed as cce:
+            print('[error] %r' % cce)
+        queue.task_done()
+
+
+def send(key, body):
+    if __channel is None:
+        init()
+
+    if key not in MSG_TO_SERVER_KEYS.__members__:
+        print("[error] %r is not a valid message key to send to the server" % key)
+    else:
+        queue.put({'key': key, 'body': body})
